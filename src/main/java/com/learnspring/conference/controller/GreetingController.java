8380089d001d3b46f28bdf876733aedf32f248fa
lucas <lugarcai@asjdfl>
2022-02-19 23:22:49 +0100

    first upload, introduction of Tomcat for work with JSP

---
+++ src/main/java/com/learnspring/conference/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.learnspring.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+    @GetMapping("greeting")
+    public String greeting(Map<String, Object> model){
+        model.put("message", "Hello Bryan");
+        return "greeting";
+    }
+}
